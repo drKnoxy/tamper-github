e60d4b77a1788ed9e2fbdc815962cca6bab0a5ee
Adam Knox <adam.w.knox@gmail.com>
2015-11-10 11:08:51 -0500

    add button for toggling whitespace

--- main.js
+++ main.js
@@ -8,45 +8,175 @@
 // @grant        none
 // ==/UserScript==
 
-var blobSelector = '.blob-wrapper, .render-wrapper, .file-header + .empty';
-function monitorHeaderToggle() {
-  $(document).on('click', '.file-header', function(e){
-    $(this).next(blobSelector).toggle();
-  });
-}
+var tool = (function() {
 
-var toggleID = 'js-blob-collapser';
-function addToggle() {
-  if (!$('#'+toggleID).length){
-    $('#toc .btn-group').before('<a id="'+toggleID+'" class="btn btn-sm right" style="margin-left:4px;">Collapse</a>');
-  }
-}
+    var collapse = {
+        // vars
+        toggleId: 'js-blob-toggle',
+        blobSelector: '.blob-wrapper, .render-wrapper, .file-header + .empty',
+        isAllVisible: true,
 
-var allVisible = true;
-function monitorToggle() {
-  $(document).on('click', '#'+toggleID, function(e) {
-    e.preventDefault();
-    if (allVisible) {
-      $(this).addClass('selected');
-      $(blobSelector).hide();
-    } else {
-      $(this).removeClass('selected');
-      $(blobSelector).show();
+        // methods
+        addElement: collapseAddElement,
+        monitor: collapseMonitor
+    };
+
+    var whitespace = {
+        // vars
+        toggleId: 'js-whitespace-toggle',
+        isVisible: false,
+
+        // methods
+        init: whitespaceInit,
+        addElement: whitespaceAddElement,
+        monitor: whitespaceMonitor
+    };
+
+
+    // Public methods
+    return {
+        init: init,
+        addElements: addElements
     }
 
-    allVisible = !allVisible;
-  });
-}
 
-$(function(){
-  // Toggling with the header
-  monitorHeaderToggle();
+    /////////////////////
 
-  // Collapsing all
-  addToggle();
-  monitorToggle();
+    function init() {
+        whitespace.init();
 
-  $(document).on('pjax:complete pjax:popstate', function(e){
-    addToggle();
-  });
+        // Watch for events
+        collapse.monitor();
+        whitespace.monitor();
+    }
+
+    function addElements() {
+        collapse.addElement();
+        whitespace.addElement();
+    }
+
+
+    //////////////////////////
+    /// Whitespace Methods
+    //////////////////////////
+
+    function whitespaceInit() {
+        var search = _getSearchAsObj();
+        whitespace.isVisible = (search.w == 1);
+    }
+
+    function whitespaceMonitor() {
+        $(document).on('click', '#' + whitespace.toggleId, function(e) {
+            e.preventDefault();
+
+            // blow away the whole search query...
+            window.location.search = whitespace.isVisible ? 'w=0' : 'w=1';
+        });
+    }
+
+    function whitespaceAddElement() {
+        var toggle = {
+            id: whitespace.toggleId,
+            label: 'Ignore Whitespace',
+            isSelected: whitespace.isVisible
+        };
+
+        _addToggle(toggle);
+    }
+
+
+    //////////////////////////
+    /// Collapse Methods
+    //////////////////////////
+
+    function collapseMonitor() {
+        collapseMonitorHeader();
+        collapseMonitorToggle();
+    }
+
+    function collapseMonitorHeader() {
+        $(document).on('click', '.file-header', function(e) {
+            $(this).next(collapse.blobSelector).toggle();
+        });
+    }
+
+    function collapseMonitorToggle() {
+        $(document).on('click', '#' + collapse.toggleId, function(e) {
+            e.preventDefault();
+
+            // can't use toggle, because we need to obey our state
+            // not the items state
+            if (collapse.isAllVisible) {
+                $(this).addClass('selected');
+                $(collapse.blobSelector).hide();
+            } else {
+                $(this).removeClass('selected');
+                $(collapse.blobSelector).show();
+            }
+
+            collapse.isAllVisible = !collapse.isAllVisible;
+        });
+    }
+
+    function collapseAddElement() {
+        var toggle = {
+            id: collapse.toggleId,
+            label: 'Collapse'
+        };
+
+        _addToggle(toggle);
+    }
+
+    //////////////////////////
+    /// Utils
+    //////////////////////////
+
+    /**
+     * @param options {id, label, isSelected}
+     */
+    function _addToggle(options) {
+        if (!$('#' + options.id).length) {
+            var cssClasses = 'btn btn-sm right';
+            if (options.isSelected) {
+                cssClasses += ' selected';
+            }
+
+            var tmpl = [
+                '<a id="' + options.id + '"',
+                    'class="' + cssClasses + '"',
+                    'style="margin-left: 4px;"',
+                    '>',
+                    options.label,
+                '</a>'
+            ].join(' ');
+
+            $('#toc .btn-group').before(tmpl);
+        }
+    }
+
+    function _getSearchAsObj() {
+        var search = {};
+        if (window.location.search) {
+            window.location.search.replace('?', '').split('&').forEach(function(el) {
+                var group = el.split('=');
+                var prop = group[0];
+                var val = group[1] || '';
+                return search[prop] = val;
+            });
+        }
+
+        return search;
+    }
+
+})();
+
+// ready!
+$(function() {
+    tool.init();
+    tool.addElements();
+
+    $(document).on('pjax:complete pjax:popstate', function(e) {
+        tool.addElements();
+    });
 });
+

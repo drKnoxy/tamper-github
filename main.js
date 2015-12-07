ffcccda7b8798e8407897f11fce8fab5b95bc5f5
Adam Willoughby-Knox <adam.w.knox@gmail.com>
2015-12-07 10:48:47 -0500

    refactor a little

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name         GitHub - Make PRs easier
-// @namespace    http://adamwknox.com
+// @name         GitHub - Make PRs easier to diff
+// @namespace    https://github.com/drKnoxy/
 // @version      1.0
 // @description  Add some js buttons to diffs
 // @author       DrKnoxy
@@ -9,42 +9,110 @@
 // ==/UserScript==
 
 var tool = (function() {
+    
+    // Prep global variables
+    var isWhitespaceVisible = _isWhitespaceVisible();
 
-    var collapse = {
-        // vars
-        toggleId: 'js-blob-toggle',
-        blobSelector: '.blob-wrapper, .render-wrapper, .file-header + .empty',
-        isAllVisible: true,
+    /**
+     * The collapse headers feature
+     */
+    var collapse = (function(){
+        var toggleId = 'js-blob-toggle';
+        var blobSelector = '.blob-wrapper, .render-wrapper, .file-header + .empty';
+        var isAllVisible = true;
+        
+        return {
+            addElement: addElement,
+            monitor: monitor
+        };
 
-        // methods
-        addElement: collapseAddElement,
-        monitor: collapseMonitor
-    };
+        //////////////////////
 
-    var whitespace = {
-        // vars
-        toggleId: 'js-whitespace-toggle',
-        isVisible: false,
+        function monitor() {
+            _monitorHeader();
+            _monitorToggle();
+        }
 
-        // methods
-        init: whitespaceInit,
-        addElement: whitespaceAddElement,
-        monitor: whitespaceMonitor
-    };
+        function _monitorHeader() {
+            $(document).on('click', '.file-header', function(e) {
+                $(this).next(blobSelector).toggle();
+            });
+        }
 
+        function _monitorToggle() {
+            $(document).on('click', '#' + toggleId, function(e) {
+                e.preventDefault();
 
-    // Public methods
+                // can't use toggle, because we need to obey our state
+                // not the items state
+                if (isAllVisible) {
+                    $(this).addClass('selected');
+                    $(blobSelector).hide();
+                } else {
+                    $(this).removeClass('selected');
+                    $(blobSelector).show();
+                }
+
+                isAllVisible = !isAllVisible;
+            });
+        }
+
+        function addElement() {
+            var toggle = {
+                id: toggleId,
+                label: 'Collapse'
+            };
+
+            _addToggle(toggle);
+        }
+    })();
+
+    /**
+     * The hide whitespace features
+     */
+    var whitespace = (function(isWhitespaceVisible){
+        var toggleId = 'js-whitespace-toggle';
+        var isWhitespaceVisible = isWhitespaceVisible;
+
+        return {
+            addElement: addElement,
+            monitor: monitor
+        };
+
+        //////////////////////
+
+        function monitor() {
+            $(document).on('click', '#' + toggleId, function(e) {
+                e.preventDefault();
+
+                // blow away the whole search query...
+                window.location.search = isWhitespaceVisible ? '' : 'w=1';
+            });
+        }
+
+        function addElement() {
+            var toggle = {
+                id: toggleId,
+                label: 'Ignore Whitespace',
+                isSelected: isWhitespaceVisible
+            };
+
+            _addToggle(toggle);
+        }
+
+    })(isWhitespaceVisible);
+
+    /**
+     * Public methods
+     */
     return {
-        init: init,
+        monitor: monitor,
         addElements: addElements
     }
 
+    //////////////////////
 
-    /////////////////////
-
-    function init() {
-        whitespace.init();
-
+    function monitor() {
         // Watch for events
         collapse.monitor();
         whitespace.monitor();
@@ -55,126 +123,73 @@ var tool = (function() {
         whitespace.addElement();
     }
 
-
-    //////////////////////////
-    /// Whitespace Methods
-    //////////////////////////
-
-    function whitespaceInit() {
-        var search = _getSearchAsObj();
-        whitespace.isVisible = (search.w == 1);
-    }
-
-    function whitespaceMonitor() {
-        $(document).on('click', '#' + whitespace.toggleId, function(e) {
-            e.preventDefault();
-
-            // blow away the whole search query...
-            window.location.search = whitespace.isVisible ? 'w=0' : 'w=1';
-        });
-    }
-
-    function whitespaceAddElement() {
-        var toggle = {
-            id: whitespace.toggleId,
-            label: 'Ignore Whitespace',
-            isSelected: whitespace.isVisible
-        };
-
-        _addToggle(toggle);
-    }
-
-
-    //////////////////////////
-    /// Collapse Methods
-    //////////////////////////
-
-    function collapseMonitor() {
-        collapseMonitorHeader();
-        collapseMonitorToggle();
-    }
-
-    function collapseMonitorHeader() {
-        $(document).on('click', '.file-header', function(e) {
-            $(this).next(collapse.blobSelector).toggle();
-        });
-    }
-
-    function collapseMonitorToggle() {
-        $(document).on('click', '#' + collapse.toggleId, function(e) {
-            e.preventDefault();
-
-            // can't use toggle, because we need to obey our state
-            // not the items state
-            if (collapse.isAllVisible) {
-                $(this).addClass('selected');
-                $(collapse.blobSelector).hide();
-            } else {
-                $(this).removeClass('selected');
-                $(collapse.blobSelector).show();
-            }
-
-            collapse.isAllVisible = !collapse.isAllVisible;
-        });
-    }
-
-    function collapseAddElement() {
-        var toggle = {
-            id: collapse.toggleId,
-            label: 'Collapse'
-        };
-
-        _addToggle(toggle);
-    }
-
-    //////////////////////////
-    /// Utils
-    //////////////////////////
-
     /**
+     * Add a button to the page
      * @param options {id, label, isSelected}
      */
     function _addToggle(options) {
-        if (!$('#' + options.id).length) {
-            var cssClasses = 'btn btn-sm right';
-            if (options.isSelected) {
-                cssClasses += ' selected';
-            }
+        var toggleTemplate = '<a id="{{id}}" class="{{cssClass}}" style="margin-left: 4px;">{{label}}</a>';
 
-            var tmpl = [
-                '<a id="' + options.id + '"',
-                    'class="' + cssClasses + '"',
-                    'style="margin-left: 4px;"',
-                    '>',
-                    options.label,
-                '</a>'
-            ].join(' ');
+        // Make sure it isn't on the page already
+        if (!$('#' + options.id).length) {
+            options.cssClass = 'btn btn-sm right';
+            if (options.isSelected) {
+                options.cssClass += ' selected';
+            }
+            tmpl = _render(toggleTemplate, options);
 
             $('#toc .btn-group').before(tmpl);
         }
     }
 
-    function _getSearchAsObj() {
+    function _isWhitespaceVisible() {
         var search = {};
         if (window.location.search) {
             window.location.search.replace('?', '').split('&').forEach(function(el) {
                 var group = el.split('=');
                 var prop = group[0];
                 var val = group[1] || '';
-                return search[prop] = val;
+                search[prop] = val;
             });
         }
 
-        return search;
+        return (search.w && parseInt(search.w, 10) == 1);
+    }
+
+    /**
+     * Simple template rendering
+     * double curly brace syntax {{id}} or {{ id }}
+     * 
+     * @param  {string} tmpl
+     * @param  {obj}    data  [description]
+     * @return {string}
+     */
+    function _render(tmpl, data) {
+        var curlyBraceRegex = /{{([^}}]+)}}/g
+        var finds = tmpl.match(curlyBraceRegex);
+
+        finds.forEach(function(curlyProp) {
+            var prop = curlyProp.replace('{{', '').replace('}}','').trim();
+
+            if (data[prop]){
+                tmpl = tmpl.split(curlyProp).join(data[prop]);
+            }
+        });
+
+        return tmpl;
     }
 
 })();
 
 // ready!
 $(function() {
-    tool.init();
+    // Make sure monitoring is going
+    tool.monitor();
+
+    // Add elements on page load
     tool.addElements();
 
+    // Add elements on page change
     $(document).on('pjax:complete pjax:popstate', function(e) {
         tool.addElements();
     });
